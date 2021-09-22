69b44e36d0f53e7bb577197cab9d4116fa33fe01
Tanous Azzi <tanous@protonmail.com>
2021-09-22 20:39:57 +0000

    Convert to object-oriented Javascript

--- player.js
+++ player.js
@@ -1,21 +1,21 @@
-function configurarVideo(id) {
-    const video = document.getElementById(id)
-    const botaoPlay = document.querySelector(`[w-play=${id}]`)
-    
-    const source = document.getElementById('source')
-    const src = source.getAttribute("src")
-    const playlist = document.getElementsByTagName("li")
-    
-    botaoPlay.onclick =
-    function playPause() {
-        if(video.paused) {
-            botaoPlay.innerHTML = '<img src="./imgs/pause.svg" width="35" alt="pause" id="pause">'
-            video.play()
-        } else {
-            botaoPlay.innerHTML = '<img src="./imgs/play.svg" width="35" alt="play" id="play">'
-            video.pause()
-        }
-    }    
+function VideoPlayer(id) {
+    this.video = document.getElementById(id)
+    this.mainButton = document.querySelector(`[w-play=${id}]`)
+    this.source = document.getElementById('source')
+    this.src = source.getAttribute("src")
+    this.playlist = document.getElementsByTagName("li")
+
+    this.mainButton.onclick = function(){playPause()};
 }
 
-configurarVideo('playerVideo')
+function playPause() {
+    if(wari.video.paused) {
+        wari.mainButton.innerHTML = '<img src="./imgs/pause.svg" width="35" alt="pause" id="pause">'
+        wari.video.play()
+    } else {
+        wari.mainButton.innerHTML = '<img src="./imgs/play.svg" width="35" alt="play" id="play">'
+        wari.video.pause()
+    }
+}
+
+const wari =  new VideoPlayer('playerVideo')
